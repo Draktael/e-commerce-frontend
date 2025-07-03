2086612d90c04439d31418dca1454a5cca7af9a0
Alexander Benjamin <darekx@Alexanders-MacBook-Air.local>
2025-07-02 20:13:03 -0400

    🚀Login form terminado + Logica del inicio de sesion completa

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 import SuccessPage from './components/SucessPage';
+import LoginForm from './components/LoginForm';
+import axios from 'axios';
 
 function App() {
-  return (
+  const [user, setUser] = useState(null);
+  //carga usuario si hay token
+
+  const loadUser = async token => {
+    try {
+      const res = await axios.get('http://localhost:8000/api/me/', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+    console.log("Usuario cargado:", res.data);
+    setUser(res.data);
+  } catch (err) {
+    console.error("Error al cargar usuario:", err.response?.data || err.message);
+  }
+};
+
+ const handleLogin = async (username,password) => {
+  try {
+    const res = await axios.post('http://localhost:8000/api/token/', {
+      username,
+      password,
+    });
+
+    const token = res.data.access;
+    localStorage.setItem('accessToken', token);
+    loadUser(token);
+  } catch (err) {
+    console.error("Login fallido:", err.response?.data || err.message);
+  }
+};
+
+  useEffect(() => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    loadUser(token);
+  }
+}, []);
+
+
+
+  return(
     <Router>
       <div>
         <h1>Tienda Online</h1>
+        <nav>
+          <Link to="/">Inicio</Link> | <Link to="/cart">Carrito</Link>
+          {user ? (
+            <>
+              <span> | Bienvenido, {user.username}</span>
+            </>
+          ) : (
+            <Link to="/login"> | Iniciar sesión</Link>
+          )}
+        </nav>
+
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/success" element={<SuccessPage />} />
+          <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const LoginForm = ({ onLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        onLogin(username, password);
+    };
+
+    return  (
+        <form onSubmit={handleSubmit}>
+            <h3>Iniciar Sesión</h3>
+            <input
+                type="text"
+                placeholder="Usuario"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                /><br />
+            <input
+                type="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                /><br />
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    );
+};
+
+export default LoginForm;
